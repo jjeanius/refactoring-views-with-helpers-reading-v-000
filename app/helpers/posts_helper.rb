16facdcfe58fa0ce04314d7536a703456a7fcd02
Judy McGoldrick <jjeanius@verizon.net>
2019-02-03 04:52:53 +0000

    Automatically backed up by Learn

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,3 +1,7 @@
 module PostsHelper
-  post.updated_at.strftime("Last updated %A, %b %e, at %1:%M %P")
+
+  def last_updated(post)
+    post.updated_at.strftime("Last updated %A, %b %e, at %1:%M %P")
+  end
+
 end
